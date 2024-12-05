177c332a4cc6c18dbca6279e4a71f10c14686569
Laura Fumagalli <laura.rodrigues@zup.com.br>
2024-12-05 09:49:44 -0300

    feat: add main with operations

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int numero = 10;
+        numero += 5; // Equivale a numero = numero + 5
+        numero -= 3; // Equivale a numero = numero - 3
+        numero *= 2; // Equivale a numero = numero * 2
+        numero /= 4; // Equivale a numero = numero / 4
+        numero %= 2; // Equivale a numero = numero % 2
+//        System.out.println(numero);
+
+        Integer numeroInteiro = 10;
+        Double numeroDecimal = 5.5;
+        Boolean verdadeiro = true;
+
+        int a = 10;
+        int b = 20;
+//        System.out.println("a == b ? " + (a == b));
+//        System.out.println("a != b ? " + (a != b));
+//        System.out.println("a > b ? " + (a > b));
+//        System.out.println("a < b ? " + (a < b));
+//        System.out.println("a >= b ? " + (a >= b));
+//        System.out.println("a <= b ? " + (a <= b));
+        int n = 123;
+        String numeroComoString1 = String.valueOf(n);
+        String numeroComoString2 = Integer.toString(n);
+        System.out.println(numeroComoString1.getClass());
+        System.out.println(numeroComoString2.getClass());
+
+        int numeroConvertido = Integer.parseInt(numeroComoString2);
+        System.out.println(numeroConvertido);
+
+        if(a == b) {
+            System.out.println("a é igual a b");
+        } else if(a > b) {
+            System.out.println("a é maior que b");
+        } else {
+            System.out.println("a é diferente e menor que b");
+        }
+
+        System.out.println("Laura println");
+        System.out.print("laura print");
+        System.out.printf("A pessoa %s tem %s anos", "flavia");
     }
 }
